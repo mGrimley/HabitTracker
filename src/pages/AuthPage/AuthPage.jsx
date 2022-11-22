322fe63f18cb8fd62747472a5d0621e72d89505d
mGrimley <mich@elgrimley.com>
2022-11-21 21:33:21 -0800

    Modifies AuthPage

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,12 +1,15 @@
+import { useState } from 'react'
 import SignUpForm from '../../components/SignUpForm/SignUpForm'
 import LoginForm from '../../components/LoginForm/LoginForm'
 
 export default function AuthPage({ setUser }) {
+    const [showLogin, setShowLogin] = useState(true)
+
     return (
         <main>
             <h1>AuthPage</h1>
-            <SignUpForm setUser={setUser} />
-            <LoginForm setUser={setUser}/>
+            <button onClick={() => setShowLogin(!showLogin)}>{showLogin ? 'SIGN UP' : 'LOG IN'}</button>
+            {showLogin ? <LoginForm setUser={setUser}/> : <SignUpForm setUser={setUser} />}
         </main>
     )
 }
