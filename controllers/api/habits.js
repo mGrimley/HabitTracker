b59206e2d061a8803dbb3c7fd976ae4f6cb50d00
mGrimley <mich@elgrimley.com>
2022-11-30 15:56:49 -0800

    Refactors Habit controller functions

--- controllers/api/habits.js
+++ controllers/api/habits.js
@@ -8,22 +8,38 @@ module.exports = {
 }
 
 async function index(req, res) {
-    const habits = await Habit.find({});
-    res.status(200).json(habits);
+    try {
+        const habits = await Habit.find({user: req.user._id});
+        res.status(200).json(habits);
+    } catch (err) {
+        res.status(400).json(err);
+    }
 }
 
 async function create(req, res) {
-    const habit = await Habit.create(req.body);
-    res.status(201).json(habit);
+    try {
+        const habit = await Habit.create(req.body);
+        res.status(201).json(habit);
+    } catch (err) {
+        res.status(400).json(err);
+    }
 }
 
 async function deleteOne(req, res) {
-    const deletedHabit = await Habit.findByIdAndRemove(req.params.id);
-    res.status(200).json(deletedHabit);
+    try {
+        const habit = await Habit.findByIdAndDelete(req.params.id);
+        res.status(200).json(habit);
+    } catch (err) {
+        res.status(400).json(err);
+    }
 }
 
 async function update(req, res) {
-    const updatedHabit = await Habit.findByIdAndUpdate
-    (req.params.id, req.body , {new: true});
-    res.status(200).json(updatedHabit);
+    try {
+        const habit = await Habit.findByIdAndUpdate(req.params.id, req.body, {new: true});
+        // await habit.save();
+        res.status(200).json(habit);
+    } catch (err) {
+        res.status(400).json(err);
+    }
 }
